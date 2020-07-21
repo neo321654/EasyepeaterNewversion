57ca789a8d3f352573f7db31820a15f83a50ed76
neo321654 <neo321654@rambler.ru>
2020-07-21 17:45:56 +0300

    разбираюсь с вебвью

---
+++ app/src/main/java/com/spotolcom/easyrepeater/ui/webview/WebviewViewModel.kt
@@ -0,0 +1,7 @@
+package com.spotolcom.easyrepeater.ui.webview
+
+import androidx.lifecycle.ViewModel
+
+class WebviewViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/spotolcom/easyrepeater/ui/webview/webview.kt
@@ -0,0 +1,67 @@
+package com.spotolcom.easyrepeater.ui.webview
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.spotolcom.easyrepeater.R
+import kotlinx.android.synthetic.main.webview_fragment.*
+
+
+class webview : Fragment() {
+    lateinit var webView: WebView
+    companion object {
+        fun newInstance() = webview()
+    }
+
+    private lateinit var viewModel: WebviewViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+       val view :View = inflater.inflate(R.layout.webview_fragment, container, false)
+      //  web_view.setloadUrl("https://www.avito.ru/")
+
+        webView = view.findViewById(R.id.web_view)
+
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(WebviewViewModel::class.java)
+        // TODO: Use the ViewModel
+
+    //    CookieSyncManager.createInstance(this)
+//        val cookieManager: CookieManager = CookieManager.getInstance()
+//        cookieManager.setAcceptCookie(true)
+//
+//        val webview = WebView(context)
+//        val ws = webview.settings
+//        ws.saveFormData = true
+//        ws.savePassword = true // Not needed for API level 18 or greater (deprecated)
+//
+//
+    //    WebAction(webview)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl("https://www.avito.ru/")
+
+
+    }
+
+
+
+
+}
