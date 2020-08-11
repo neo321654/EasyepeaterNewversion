a5cfdb7c1e121b6647dfd6726ef687fb03a447e6
Neo321654 <neo321654@rambler.ru>
2020-08-11 15:16:19 +0300

    signature234

--- app/src/main/java/com/spotolcom/easyrepeater/MainActivity.kt
+++ app/src/main/java/com/spotolcom/easyrepeater/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     //my
     //test22
     //test2
+    //test24
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
