3a77b68dc4c94db46967faaa96474ffd4025e47b
neo321654 <neo321654@rambler.ru>
2020-08-18 08:55:59 +0300

    Добавил поле в настройку со временем повторов, ещё никак её не обработал

--- app/src/main/java/com/spotolcom/easyrepeater/MySettingsFragment.kt
+++ app/src/main/java/com/spotolcom/easyrepeater/MySettingsFragment.kt
@@ -1,10 +1,26 @@
 package com.spotolcom.easyrepeater
 
 import android.os.Bundle
+import android.text.TextUtils
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class MySettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+        val time_repeat: EditTextPreference? = findPreference("time_repeat")
+
+        time_repeat?.summaryProvider =
+            Preference.SummaryProvider<EditTextPreference> { preference ->
+                val text = preference.text
+                if (TextUtils.isEmpty(text)) {
+                    "Время не установлено"
+                } else {
+                    "Интервал повторений равен: $text"
+                }
+            }
+
     }
 }
